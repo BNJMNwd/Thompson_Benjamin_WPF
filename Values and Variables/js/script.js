1dd0bad24690cf242ecb473b39ef2cc4367980e7
BNJMNwd <bthomps1988@gmail.com>
2014-01-15 17:06:39 -0500

    more values and variables

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -7,4 +7,25 @@ console.log("Hello World");
 console.log(yearBorn);
     */
 
-alert("Hello World");
+//alert("Hello World");
+
+
+var age;
+age = 25;
+
+var yearOfBirth = 1988;
+
+
+var isStudent = false;
+
+var benjamin = "My Friend";
+var firstName = "Benjamin";
+
+var phrase = 'I don\'t know \nYou\'ll have to speak to the professor';
+
+//arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
+
+
+console.log(muppetNames);
