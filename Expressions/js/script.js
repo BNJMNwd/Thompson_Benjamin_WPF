46791450f2990c0a0f272b700fd9424d2c71a9c4
BNJMNwd <bthomps1988@gmail.com>
2014-01-15 20:51:11 -0500

    expressions complete

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,1 +1,106 @@
 //Expressions
+
+var a = 2;
+
+a = a + 3;
+
+//console.log(a);
+
+
+var yearBorn = 1988;
+
+var age = 2014 - yearBorn - 1;
+
+//console.log(age);
+
+
+// Area of the triangle is half the width time the height
+
+var width = 8;
+var height = 7;
+
+var area = width * height/2;
+
+
+//console.log(area);
+
+var remainder = 211 % 2;
+
+//console.log(remainder);
+
+
+var quiz1 = 87;
+
+var quiz2 = 100;
+
+var quiz3 = 60;
+
+var quiz4 = 80;
+
+
+//the average is the sum of the items divided by the number of items
+
+var average = (quiz1 +quiz2 + quiz3 + quiz4)/4;
+
+//console.log(average);
+
+
+
+var length = 7;
+var width = 6;
+
+var perimeter = length * 2 + width * 2;
+//console.log(perimeter);
+
+
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+
+//console.log(total);
+
+
+/*var firstName = "Benjamin";
+var lastName = "Thompson";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);*/
+
+//var a = "6";
+//var b = "7";
+
+//var result = a + b;
+//console.log(result);
+
+
+
+
+//var stringVar = "6";
+
+//var result =  7 + Number(stringVar);
+//console.log(result);
+
+
+
+var areaCode = 407;
+var firstPart = 363;
+var secPart = 8000;
+
+//(407)
+var phoneNo = "(" + String(areaCode) +")" + String(firstPart) + "-" + String(secPart);
+//console.log(phoneNo);
+
+
+
+
+var a = 3;
+
+//a ++ ;
+a -- ;
+
+console.log(a);
+
+
+
