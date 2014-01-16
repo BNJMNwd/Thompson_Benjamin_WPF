3c84bcc37a4fa12cc7c5ec72722cf4d08c1c883c
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:33:53 -0500

    added calculations

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,3 +7,6 @@ var totalAt1 = 2398;    //How many people were in the park at 1pm?
 var entered = 367;      //How many people came into the park between 1pm and 2pm?
 var exits = 126;        //How many people left the park between 1pm and 2pm?
 
+inPark = totalAt1 + entered - exits;
+
+console.log();
