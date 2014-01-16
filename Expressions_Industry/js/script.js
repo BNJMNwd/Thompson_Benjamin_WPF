f04d241731135bace379f6d5b310f9c0aaa1e3e5
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:31:11 -0500

    added comments

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,8 @@
 //Benjamin Thompson 1/16/14 Expressions
+
+
+//How many people were in the Theme park  at 2pm?
+
+//How many people were in the park at 1pm?
+//How many people came into the park between 1pm and 2pm?
+//How many people left the park between 1pm and 2pm?
