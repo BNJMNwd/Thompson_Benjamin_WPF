60190f5de2397241d66b4a4f2cdf485cdf6cc647
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 09:58:53 -0500

    finished sparky and began pie part 1

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -6,9 +6,15 @@
 
 //Calculate how old sparky is in dog years based on his age in human years
 
-var sparkyAge = 2;      //What is Sparky's age in human years?
+var sparkyAge = 4;      //What is Sparky's age in human years?
 
-var dogAge = sparkyAge * 7; // 
+var dogAge = sparkyAge * 7; // Multiples Sparky's age in human years by 7 which gives us his age is dog years.
 
 
-console.log("Sparky is " + sparkyAge +  " human years which is " + dogAge + " in dog years");
+//This is what is printed
+
+console.log("Sparky is " + sparkyAge +  " human years which is " + dogAge + " in dog years.");
+
+
+//Slice of pie part 1
+
