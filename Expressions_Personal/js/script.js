ba0f1609fd39e87bb563dc7398ceb5e35e87ee1c
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:22:51 -0500

    fixed formula to average and removed unneeded var

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,14 +4,16 @@
 
 //How long will it take to pack the last room of my house?
 
-var numberOfRooms = 5;              //How many rooms are in your house?
 var numberOfRoomsPacked = 4;        //How many of these rooms have you packed?
 var rooms = [6, 4, 6, 7];           //How many hours did it take you to pack these rooms?
 
-var roomTotal = rooms[0] +rooms[1] + rooms[2] + rooms[3];
+var roomTotal = rooms[0] +rooms[1] + rooms[2] + rooms[3]; //Calculation to get the total number of hours packed
 
-var roomAver = roomTotal / numberOfRooms;
+var roomAver = roomTotal / numberOfRoomsPacked;   //Calculation to find the average amount of time to pack a room
+
+
+//This is what will be displayed
 
 
 console.log("It took you about " + roomAver + " hours to pack the last " + numberOfRoomsPacked +
-    " rooms of your house. You will need to set aside " + roomAver + " hours to pack the last room");
+    " rooms of your house. You will need to set aside " + roomAver + " hours to pack the next room");
