a46257eddc09c26daa47561d6cf3ac75e4744305
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:36:16 -0500

    added console.log

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,6 +7,7 @@ var totalAt1 = 2398;    //How many people were in the park at 1pm?
 var entered = 367;      //How many people came into the park between 1pm and 2pm?
 var exits = 126;        //How many people left the park between 1pm and 2pm?
 
-inPark = totalAt1 + entered - exits;
+inPark = totalAt1 + entered - exits; //The number of people in the park was the previous total plus the entries minus the exits.
 
-console.log();
+
+console.log("At 2pm we had " + inPark + " customers in the park!");
