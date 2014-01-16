982be6b035ea17f89c0aac7e5e422be17fbd9b82
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:17:13 -0500

    added console.log

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,9 +4,15 @@
 
 
 
-
+var numberOfRooms = 5;
+var numberOfRoomsPacked = 4;
 var rooms = [6, 4, 6, 7];
 
-var roomAver = rooms[0] +rooms[1] + rooms[2] + rooms[3];
+var roomTotal = rooms[0] +rooms[1] + rooms[2] + rooms[3];
 
-//It took you about x hours to pack the last 4 rooms of your house. You will need to set aside x hours to pack the last room
+var roomAver = roomTotal / numberOfRooms;
+
+
+console.log("It took you about " + roomAver + " hours to pack the last " + numberOfRoomsPacked +
+    " rooms of your house. You will need to set aside " + roomAver + " hours to pack the last room");
+//It took you about x hours to pack the last x rooms of your house. You will need to set aside x hours to pack the last room
