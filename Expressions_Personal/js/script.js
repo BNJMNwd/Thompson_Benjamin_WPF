47f4f56cf31721b3e3641d18c174d6d475c53e2c
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:19:38 -0500

    added comments

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,11 +2,11 @@
 
 
 
+//How long will it take to pack the last room of my house?
 
-
-var numberOfRooms = 5;
-var numberOfRoomsPacked = 4;
-var rooms = [6, 4, 6, 7];
+var numberOfRooms = 5;              //How many rooms are in your house?
+var numberOfRoomsPacked = 4;        //How many of these rooms have you packed?
+var rooms = [6, 4, 6, 7];           //How many hours did it take you to pack these rooms?
 
 var roomTotal = rooms[0] +rooms[1] + rooms[2] + rooms[3];
 
@@ -15,4 +15,3 @@ var roomAver = roomTotal / numberOfRooms;
 
 console.log("It took you about " + roomAver + " hours to pack the last " + numberOfRoomsPacked +
     " rooms of your house. You will need to set aside " + roomAver + " hours to pack the last room");
-//It took you about x hours to pack the last x rooms of your house. You will need to set aside x hours to pack the last room
