700b9be2fed7ab60fbff2b36ecc45f0cfadbf744
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:43:48 -0500

    completed industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,11 +3,14 @@
 
 //How many people were in the Theme park  at 2pm?
 
-var totalAt1 = 2398;    //How many people were in the park at 1pm?
-var entered = 367;      //How many people came into the park between 1pm and 2pm?
-var exits = 126;        //How many people left the park between 1pm and 2pm?
+var previousHour = 14056;    //How many people were in the park at 1pm?
+var entered = 1230;      //How many people came into the park between 1pm and 2pm?
+var exits = 367;        //How many people left the park between 1pm and 2pm?
 
-inPark = totalAt1 + entered - exits; //The number of people in the park was the previous total plus the entries minus the exits.
+var inPark = previousHour + entered - exits; //The number of people in the park was the previous total plus the entries minus the exits.
 
+var increase = inPark - previousHour; //A calculation to tell the difference between the number of customers since last hour
 
-console.log("At 2pm we had " + inPark + " customers in the park!");
+//What will be displayed
+
+console.log("At 2pm we had " + inPark + " customers in the park! This means we had an increase of " + increase +  " customers since 1pm!");
