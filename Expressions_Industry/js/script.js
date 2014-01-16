304c2989bc50e7703cbcf98a89dc469770ee6f08
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:32:39 -0500

    entered givens

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,6 +3,7 @@
 
 //How many people were in the Theme park  at 2pm?
 
-//How many people were in the park at 1pm?
-//How many people came into the park between 1pm and 2pm?
-//How many people left the park between 1pm and 2pm?
+var totalAt1 = 2398;    //How many people were in the park at 1pm?
+var entered = 367;      //How many people came into the park between 1pm and 2pm?
+var exits = 126;        //How many people left the park between 1pm and 2pm?
+
