e0e873720611bc98d929b490ed01fbb4b77e0884
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:10:03 -0500

    changed questions and givens

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,12 @@
 //Benjamin Thompson 1/16/14 Expressions
 
-//Will I have enough boxes to move from my apartment?
 
-var numberOfBoxes;
-var numberRooms;
-var boxesPerRoom;
+
+
+
+
+var rooms = [6, 4, 6, 7];
+
+var roomAver = rooms[0] +rooms[1] + rooms[2] + rooms[3];
+
+//It took you about x hours to pack the last 4 rooms of your house. You will need to set aside x hours to pack the last room
