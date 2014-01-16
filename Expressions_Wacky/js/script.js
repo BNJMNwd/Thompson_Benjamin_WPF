82fc956dceb1886912dd90cd35cabe4d721bd54d
BNJMNwd <bthomps1988@gmail.com>
2014-01-16 11:59:47 -0500

    Added comments

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,8 @@
-//Benjamin Thompson 1/16/14 Expressions 
+//Benjamin Thompson 1/16/14 Expressions
+
+//How many display cases will I need?
+
+
+//Number of comics purchased per day?
+//Number of week I will be purchasing them?
+//Number of comics that can fit in your display case?
