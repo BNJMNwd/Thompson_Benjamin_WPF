d38f0414f27bb6260a1dd6aa1e425b77144293a3
BNJMNwd <bthomps1988@gmail.com>
2014-01-22 16:22:21 -0500

    conditionals

---
+++ Conditionals/js/script.js
@@ -0,0 +1,32 @@
+//Conditionals Screencast
+
+
+/*if(condition){
+    actions
+
+}else{
+
+}
+
+
+if(sunny){
+    goToTheBeach
+}else{
+    goToTheMovies
+}
+ */
+
+
+
+var kidHeight = 40;
+var minHeight = 48;
+//If the child is old enough, print to the console "you can ride!"
+//If the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //Code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("What come after?")
+
+
+
