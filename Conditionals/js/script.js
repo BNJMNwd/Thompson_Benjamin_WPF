d11f446e184dded6f7cc019cf4083f97394005d0
BNJMNwd <bthomps1988@gmail.com>
2014-01-22 17:38:36 -0500

    1

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -17,16 +17,23 @@ if(sunny){
  */
 
 
-
-var kidHeight = 40;
+/*
+var kidHeight = 50;
 var minHeight = 48;
+var wParenetHeight = 45; //The height of the kid with a parent
+
 //If the child is old enough, print to the console "you can ride!"
 //If the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //Code performed if condition is true
+    //You can ride
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParenetHeight){
+    //You can ride with a parent present
+    console.log("You can ride, but only with a parent present");
+    }else{
+    //Sorry you have growing to do
+    console.log("Sorry kid you've got some growing to do first")
 }
-console.log("What come after?")
-
+*/
 
 
