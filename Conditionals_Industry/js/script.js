2f2ae87cac4aa98cbf6bcb3badd916a2df6bd92c
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 17:00:54 -0500

    second part of question

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,9 +2,9 @@
 
 
 var ageOfComputer = prompt("How many years have you had your computer?");
-var computer;
+var computerVirus = prompt("How many minutes does it take for your computer take to start up?");
 
-computerMessage = (ageOfComputer > 2 ) ? "You get a new computer!!" : "You do not get a new computer yet .";
+computerMessage = (ageOfComputer > 2 || computerVirus > 7 ) ? "You get a new computer!!" : "You do not get a new computer yet.";
 
 
 console.log(computerMessage);
