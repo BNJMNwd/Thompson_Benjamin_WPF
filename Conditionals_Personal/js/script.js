c725d05b604aee4801c8644faa146b3575ae0461
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 17:38:17 -0500

    added variables

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,14 @@
 //Benjamin Thompson 1/23/14 Conditionals Personal
+
+
+//Am I ready to move to my new place?
+
+var rooms = 8;
+var gasNeeded = 12;
+
+
+
+//Are all of my rooms packed? AND is the moving truck fueled up and ready to go?
+
+var howManyRooms = prompt("How many rooms have been packed?");
+var howMuchGas = prompt("How many gallons of gas our in the car?")
