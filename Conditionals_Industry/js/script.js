5e8c715e84d344e61a911eae529b0dc7152831f8
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 16:58:15 -0500

    part 1 of industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,10 @@
 //Benjamin Thompson 1/23/14 Conditionals Industry
+
+
+var ageOfComputer = prompt("How many years have you had your computer?");
+var computer;
+
+computerMessage = (ageOfComputer > 2 ) ? "You get a new computer!!" : "You do not get a new computer yet .";
+
+
+console.log(computerMessage);
