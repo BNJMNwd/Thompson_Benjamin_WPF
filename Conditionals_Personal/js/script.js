c80dfb3a9904d5390b0d4935c7dbbbb5906a2cb7
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 17:46:51 -0500

    Added print outs and started validation

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -11,4 +11,19 @@ var gasNeeded = 12;
 //Are all of my rooms packed? AND is the moving truck fueled up and ready to go?
 
 var howManyRooms = prompt("How many rooms have been packed?");
-var howMuchGas = prompt("How many gallons of gas our in the car?")
+var howMuchGas = prompt("How many gallons of gas our in the truck?")
+
+
+if(){
+
+
+}else if(){
+
+
+}else if(howManyRooms >= rooms && howMuchGas >= gasNeeded) {
+
+    console.log("Good job! You are ready to move!")
+}else{
+
+    console.log("Some more prep is needed before the big move!")
+}
