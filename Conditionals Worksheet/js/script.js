e374d78742fa59be2a92c4df79e945adb35c3c6a
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 15:25:14 -0500

    question 1

--- Conditionals Worksheet/js/script.js
+++ Conditionals Worksheet/js/script.js
@@ -1,1 +1,25 @@
 //Conditionals Worksheet
+
+
+//Last Chance for Gas!
+
+var mpg = 32;
+var gaugeReading = 50;
+var tank = 14;
+
+var distance = 200;
+
+var testTest = mpg * tank;
+
+
+
+if(mpg * tank >= distance  ){
+    //You do not need to stop for gas!
+    console.log("Yes, you can make it without stopping for gas!");
+}else{
+    //You do need to stop for gas
+    console.log("You only have " + gaugeReading/tank + ' ' + "gallons of gas in your tank, better get gas now while you can!");
+}
+
+
+
