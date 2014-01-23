563b61448f03aa67e74aa2915d22374fea0e9290
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 18:51:38 -0500

    added all data

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,9 +1,30 @@
 //Benjamin Thompson 1/23/14 Conditionals Wacky
 
+//How many boxes would you need to fill an entire apartment
 
-var length = prompt();
-var width = prompt();
-var houseSize = prompt();
+var length = prompt("What is the length of your box in feet ");
+var width = prompt("What is the width of your box in feet?");
+var houseSize = prompt("How many square feet is your apartment");
 
 var boxSpace = length * width;
+
+var boxesNeeded = houseSize / boxSpace;
+
+if(length === "" || width === "" || houseSize === ""){
+    alert("You forgot to input something! Let's try again.");
+
+
+    var length = prompt("What is the length of your box in feet ");
+    var width = prompt("What is the width of your box in feet?");
+    var houseSize = prompt("How many square feet is your apartment")
+
+    var boxSpace = length * width;
+    var boxesNeeded = houseSize / boxSpace;
+
+
+    console.log("If your apartment is " + houseSize + " square feet, you will need " + boxesNeeded + " boxes to completely fill your house!");
+
+}else{
+    console.log("If your apartment is " + houseSize + " square feet, you will need " + boxesNeeded + " boxes to completely fill your house!");
+}
 
