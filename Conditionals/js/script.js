780ed153774750afd352af0a6473b4e365393456
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 10:24:21 -0500

    conditionals

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -37,3 +37,49 @@ if(kidHeight > minHeight){
 */
 
 
+/*
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+//if the price of phone is less then our budget AND our paycheck is over 300
+
+if(iPhonePrice < budget || wonLottery === true){
+   console.log("We can but the phone!")
+}else{
+    console.log("No phone for you!")
+
+}*/
+
+/*
+var gpa = 48;
+
+//If the GPA is over the min 2.0 score the student can graduate
+
+/*if(gpa > 2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA is too low!");
+}
+
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+
+ */
+
+
+
+//If the child is under 10 they get Green Eggs and Ham, otherwise they get The Time Machine
+/* if(age < 10){
+    book = "Green Eggs and Ham";
+}else{
+    book = "The Time Machine";
+}*/
+
+
+var age = 6;
+var book;
+
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+
+
+console.log(book);
