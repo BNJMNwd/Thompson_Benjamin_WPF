a2cc609c04f6ed8bd2bc672528fa4af7ea098df5
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 17:10:59 -0500

    validation for industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,10 +1,32 @@
 //Benjamin Thompson 1/23/14 Conditionals Industry
 
 
-var ageOfComputer = prompt("How many years have you had your computer?");
-var computerVirus = prompt("How many minutes does it take for your computer take to start up?");
+var ageOfComputer = prompt("How many years have you had your computer?");   //The user enters how many years they have had a computer
+var computerVirus = prompt("How many minutes does it take for your computer take to start up?");    //The user enters how many minutes it takes to start up its computer
 
+if(ageOfComputer === ""){
+
+    alert("You forgot to input something!");
+    var ageOfComputer = prompt("How many years have you had your computer?");
+
+
+    computerMessage = (ageOfComputer > 2 || computerVirus > 7 ) ? "You get a new computer!!" : "You do not get a new computer yet.";
+
+
+    console.log(computerMessage);
+}else if(computerVirus === ""){
+
+    alert("You forgot to input something!");
+    var computerVirus = prompt("How many minutes does it take for your computer take to start up?")
+
+
+    computerMessage = (ageOfComputer > 2 || computerVirus > 7 ) ? "You get a new computer!!" : "You do not get a new computer yet.";
+
+
+    console.log(computerMessage);
+}else{
 computerMessage = (ageOfComputer > 2 || computerVirus > 7 ) ? "You get a new computer!!" : "You do not get a new computer yet.";
 
 
 console.log(computerMessage);
+}
