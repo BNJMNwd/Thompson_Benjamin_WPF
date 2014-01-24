e5343a8ec9b4fa6140d3afbc4d3ef4a568db6e83
BNJMNwd <bthomps1988@gmail.com>
2014-01-23 19:10:11 -0500

    Fixed gas and final

--- Conditionals Worksheet/js/script.js
+++ Conditionals Worksheet/js/script.js
@@ -3,24 +3,23 @@
 
 //Last Chance for Gas!
 
-var mpg = 32;       //MPG of the vehicle
-var gaugeReading = 50;      //The percentage of gas left in the tank
-var tank = 14;      //The amount of gas the tank can hold in gallons
+var mpg =32;              //MPG of the vehicle
+var gaugeReading = 40;     //The percentage of gas left in the tank
+var tank = 14;             //The amount of gas the tank can hold in gallons
 
-var distance = 200;     //The distance you need to travel
+var distance = 200;        //The distance you need to travel
 
 var testTest = mpg * tank;
 
 
 
-if(mpg * tank >= distance  ){
+if(gaugeReading / 100 * tank * mpg >= distance){ //The gas in the tank multiplied by the mpg multiplied by the distance 
     //You do not need to stop for gas!
     console.log("Yes, you can make it without stopping for gas!");
 }else{
     //You do need to stop for gas
     console.log("You only have " + gaugeReading/tank + ' ' + "gallons of gas in your tank, better get gas now while you can!");
 }
-
 
 
 //Check the Login
@@ -66,3 +65,4 @@ if(tire1 === tire2 && tire3 === tire4){ // if the pressure in tire 1 and 2 match
     //You need to get your tires checked out
     console.log("Get your tires checked out!")
 }
+
