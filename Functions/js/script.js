ca272b6d1e9aa3e0883106af34242d3bf824318a
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 14:41:59 -0500

    Functions

---
+++ Functions/js/script.js
@@ -0,0 +1,82 @@
+//Functions
+
+
+
+
+/*Function functionName (){
+ //code the function runs
+ }*/
+
+/*
+function outputMsg(){
+    console.log("Hello World")
+}
+
+
+var width = 5;
+
+
+function calcArea (){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+
+
+calcArea();
+
+
+
+
+calcArea(30, 20);
+
+function calcArea (w, h){
+    var area = w * h;
+    console.log(area);
+}
+
+
+
+
+
+
+
+function dogYears(age){
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old");
+}
+
+var age1 = 4;
+dogYears(age1);
+dogYears(5);
+dogYears(6);
+
+
+
+
+
+
+
+var total = calcArea(30, 20);
+
+function calcArea (w, h){
+    var area = w * h;
+    return area; //Function spitting the info out
+}
+
+console.log(total);
+
+*/
+
+
+
+var calcArea = function(width, height){
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 30);
+
+console.log(calcArea );
