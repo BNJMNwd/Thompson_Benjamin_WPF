f43c3bf0405b5845d79a4db4286cfbaf93720ad6
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 16:35:24 -0500

    added some content and function info

--- Function_worksheet/js/script.js
+++ Function_worksheet/js/script.js
@@ -1,8 +1,20 @@
 //Benjamin Thompson 1/30/14 Functions Worksheet
 
+
+
 //Question 1: Circumference
 
+//Calculate the circumference of a circle
 
 
 
-//Question 2: Stung
+
+circOfCircle(20)
+
+function circOfCircle(radius){
+
+
+}
+
+
+//Question 2: Stung!
