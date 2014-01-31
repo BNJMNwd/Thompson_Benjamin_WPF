e6d30201e6425c36891f3a5c7724040b67bd1a3e
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 21:52:13 -0500

    added comments

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,12 +1,12 @@
 //Benjamin Thompson 1/30/14 Functions Wacky
 
-function handWarmers (){
-    var people = 1000;
-    var perPerson = 2;
-    var box = 100;
-    var totalNeeded = people * perPerson / box;
-    console.log(totalNeeded);
+function handWarmers (){        //Start of function
+    var people = 1000;          //Number of people in the group
+    var perPerson = 2;          //Number of hand warmers each person will get
+    var box = 100;              //Number of hand warmers in each box
+    var totalNeeded = people * perPerson / box;     //Formula for the total needed
+    console.log(totalNeeded);   //What will be printed
 }
 
 
-handWarmers();
+handWarmers();          //To invoke the function
