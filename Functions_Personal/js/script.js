01b5b0adc228635f9e65911b49ef5543df67d369
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 21:23:21 -0500

    Added variables and prompts

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,3 +2,6 @@
 
 //How many weeks will it take me to walk my dog 100 miles in my new community
 
+var daily = parseInt(prompt("How many times a day do you walk?"));
+var weekly = parseInt(prompt("How many days a week do you walk?"));
+var distance = parseInt(prompt("How many miles do you walk each time you take your dog out?"));
