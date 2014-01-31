b0f63dd68b85ad1c4e8a9a662a52258369853ec1
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 21:21:37 -0500

    for class

--- Prac/js/script.js
+++ Prac/js/script.js
@@ -1,1 +1,56 @@
-alert("This is in the js file");
+//Practice
+
+
+
+
+var daily = parseInt(prompt("How many times a day do you walk?"));
+var weekly = parseInt(prompt("How many days a week do you walk?"));
+var distance = parseInt(prompt("How many miles do you walk each time you take your dog out?"));
+
+
+var timeNeeded = howLong(daily, weekly, distance);
+
+function howLong(daily, weekly , distance){
+    var test = 100 /(distance * daily * weekly) ;
+    return test;
+}
+
+console.log("It would take you " + timeNeeded + " weeks to walk your dog 100 miles.");
+
+
+var rain = parseInt(prompt("How many centimeters of rain have you had in the last 60 minutes?"));
+var lightning = parseInt(prompt("How many miles away was the last lightning strike?"));
+
+
+
+
+var answer = weather(rain, lightning)
+
+
+
+function weather(rain, lighting){
+    if(rain <= 8 && lighting >= 5){
+        var Ok = "No, the coaster does not have to go down for weather! ";
+        return Ok;
+            }else{
+            var notOk = "Yes, the coaster has to go down for weather ";
+            return notOk;
+    }
+}
+
+console.log(answer);
+
+
+
+
+
+function handWarmers (){
+    var people = 1000;
+    var perPerson = 2;
+    var box = 100;
+    var totalNeeded = people * perPerson / box;
+    console.log(totalNeeded);
+}
+
+
+handWarmers();
