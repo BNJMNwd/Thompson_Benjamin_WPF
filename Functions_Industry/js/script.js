22c15bb88444e8f09d592a87cfa3e697841b4bfb
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 21:43:42 -0500

    added comments

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,8 +2,8 @@
 
 //Will the roller coaster have to go down for weather?
 
-var rain = parseInt(prompt("How many centimeters of rain have you had in the last 60 minutes?"));
-var lightning = parseInt(prompt("How many miles away was the last lightning strike?"));
+var rain = parseInt(prompt("How many centimeters of rain have you had in the last 60 minutes?"));       //user prompt to discover how much it has rained: How many centimeters of rain have you had in the last 60 minutes?
+var lightning = parseInt(prompt("How many miles away was the last lightning strike?"));         //User prompt to discover how close lightning is: How many miles away was the last lightning strike?
 
 var maxRain = 8;
 var safeDistance = 5;
@@ -16,7 +16,7 @@ function weather(rain, lighting){
         var Ok = "No, the coaster does not have to go down for weather! ";
         return Ok;
     }else{
-        var notOk = "Yes, the coaster has to go down for weather ";
+        var notOk = "Yes, the coaster has to go down for weather.";
         return notOk;
     }
 }
