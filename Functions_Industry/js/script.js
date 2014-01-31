cb5b883f7b53eafb570318dce81b7cee385e471a
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 21:37:27 -0500

    Added function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,3 +4,14 @@
 
 var rain = parseInt(prompt("How many centimeters of rain have you had in the last 60 minutes?"));
 var lightning = parseInt(prompt("How many miles away was the last lightning strike?"));
+
+
+function weather(rain, lighting){
+    if(rain <= 8 && lighting >= 5){
+        var Ok = "No, the coaster does not have to go down for weather! ";
+        return Ok;
+    }else{
+        var notOk = "Yes, the coaster has to go down for weather ";
+        return notOk;
+    }
+}
