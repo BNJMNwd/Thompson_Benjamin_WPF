b9b48315017aa4bba74bd3f276c88ea2e5cf8b02
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 21:36:36 -0500

    Added variables with user prompts

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,3 +2,5 @@
 
 //Will the roller coaster have to go down for weather?
 
+var rain = parseInt(prompt("How many centimeters of rain have you had in the last 60 minutes?"));
+var lightning = parseInt(prompt("How many miles away was the last lightning strike?"));
