f882f3f27604f4d37391c15ffd01c13f6a5df0b6
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 21:49:58 -0500

    Added function and display

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,1 +1,12 @@
-//Benjamin Thompson 1/30/14 Functions Wacky 
+//Benjamin Thompson 1/30/14 Functions Wacky
+
+function handWarmers (){
+    var people = 1000;
+    var perPerson = 2;
+    var box = 100;
+    var totalNeeded = people * perPerson / box;
+    console.log(totalNeeded);
+}
+
+
+handWarmers();
