f9f8cfda983698d7415d78e0b6928bad88afbb54
BNJMNwd <bthomps1988@gmail.com>
2014-01-30 21:39:33 -0500

    fixed equation and added variables

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,9 +5,12 @@
 var rain = parseInt(prompt("How many centimeters of rain have you had in the last 60 minutes?"));
 var lightning = parseInt(prompt("How many miles away was the last lightning strike?"));
 
+var maxRain = 8;
+var safeDistance = 5;
 
 function weather(rain, lighting){
-    if(rain <= 8 && lighting >= 5){
+    if(
+        rain <= maxRain && lighting >= safeDistance){
         var Ok = "No, the coaster does not have to go down for weather! ";
         return Ok;
     }else{
